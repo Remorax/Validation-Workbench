064df0ab658207fd127fafa7e50526584dd5baa5
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-04-25 19:26:44 +0530

    User dashboard built, tiling sequence adjusted

--- project/main.py
+++ project/main.py
@@ -18,13 +18,57 @@ def index():
 @main.route('/dashboard')
 @login_required
 def dashboard():
+    is_admin = False
     if current_user.email in admins:
-        return render_template('admin_dashboard.html')
-    return render_template('user_dashboard.html', name=current_user.name)
+        is_admin = True
+    return render_template('dashboard.html', is_admin=is_admin)
+
+@main.route('/responses', methods=["GET"])
+@login_required
+def show_response():
+    file_idx = request.args.get('file')
+    files = [os.path.join(UPLOAD_FOLDER, file) for file in os.listdir(UPLOAD_FOLDER)]
+    files.sort(key=os.path.getmtime, reverse=True)
+    file_path = files[int(file_idx)]
+    file = [l.split("\t") for l in open(file_path).read().split("\n")][1:]
+    print (file)
+    if current_user.email in admins:
+        return render_template('responses.html', response=file)
+
+@main.route('/load-files', methods=["GET"])
+@login_required
+def load_files():
+    files = [os.path.join(UPLOAD_FOLDER, file) for file in os.listdir(UPLOAD_FOLDER)]
+    files.sort(key=os.path.getmtime, reverse=True)
+    files_str = ""
+    # print (files)
+    for i,file in enumerate(files):
+        # print (file.split("/"), file.split("/")[-1])
+        file_id = str(int(i/3)) + "_" + str(i%3)
+        td_str = ("<td id=\'" + file_id +  "\'>" +
+                    "<a href=\'/responses?file=" + str(i) + "\'>" + 
+                        "<div id=\'" + file_id + "_name\' class=\'filename\'>" + 
+                            file.split("/")[-1] +
+                    "</div></a></td>")
+        if i%3 == 0:
+            files_str += "<tr>" + td_str
+        elif i%3 == 2:
+            files_str += td_str + "</tr>"
+        else:
+            files_str += td_str
+    extra = (2 - i%3)
+    for j in range(extra):
+        idx = i+j+1
+        file_id = str(int(idx/3)) + "_" + str(idx%3)
+        files_str += ("<td id=\'" + file_id +  "\' style=\'visibility: hidden;\'>" +
+                        "<a href=\'/responses?file=" + str(idx) + "\'>" + 
+                            "<div id=\'" + file_id + "_name\' class=\'filename\'>" + 
+                        "</div></a></td>")
+
+    return jsonify({"files": files_str, "count": len(files)})
 
 @main.route('/upload-file', methods=['POST'])
 def upload():
-    print ("yup")
     if "file" not in request.files:
         resp = jsonify({'message' : 'No file part in the request'})
         resp.status_code = 400
--- project/static/dashboard.js
+++ project/static/dashboard.js
@@ -1,11 +1,22 @@
 
 var file_index = 0;
+$( document ).ready(function() {
+    $.get('load-files', function(data){
+        // Display the returned data in browser
+        file_index = data["count"];
+        $("#files").append(data["files"]);
+    });
+});
+
 
 $(document).ready(function (e) {
     $('#files-input').on('change', function () {
         var form_data = new FormData();
         var ins = document.getElementById('files-input').files[0];
         form_data.append("file", ins);
+        if (!ins) {
+            return;
+        }
         console.log(ins.name);
         console.log("idhaarrrr") 
         $.ajax({
@@ -28,7 +39,7 @@ $(document).ready(function (e) {
                         var file_id = row + "_" + i.toString();
                         tableString += "<td id=\'" + file_id +  "\' style=\'visibility: hidden;\'>" +
                                             "<a href=\'/responses?file=" + (file_index-1) + "\'>" + 
-                                                "<div class=\'filename\'>" + ins.name + "</div>" +
+                                                "<div id=\'" + file_id + "_name\' class=\'filename\'>" + ins.name + "</div>" +
                                             "</a>" +
                                         "</td>";
                         console.log(tableString)
@@ -38,6 +49,7 @@ $(document).ready(function (e) {
                 }
                 var file_id =  row + "_" + col;
                 $("#"+file_id).css("visibility", "");
+                $("#"+file_id+"_name").text(ins.name);
                 
 
             },
